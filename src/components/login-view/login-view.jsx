e2abb6c0c97f62f1ed80bdd72b8bc8222d7475fc
unknown <reman1999@outlook.com>
2023-08-03 20:11:07 +0200

    commit

--- src/components/login-view/login-view.jsx
+++ src/components/login-view/login-view.jsx
@@ -1,6 +1,9 @@
+import React from 'react';
 import { useState } from 'react';
-import Button from 'react-bootstrap/Button';
-import Form from 'react-bootstrap/Form';
+import { Link } from 'react-router-dom';
+
+// import bootstrap components
+import { Form, Button, Row, Col } from 'react-bootstrap';
 
 export const LoginView = ({ onLoggedIn }) => {
   const [username, setUsername] = useState('');
@@ -14,55 +17,91 @@ export const LoginView = ({ onLoggedIn }) => {
       Password: password,
     };
 
-    fetch(
-      `https://myflixproject.onrender.com/login?` + new URLSearchParams(data),
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        //body: JSON.stringify(data)
-      }
-    )
-      .then((response) => response.json())
+    fetch('https://myflixproject.onrender.com/login', {
+      method: 'POST',
+      headers: new Headers({
+        'Content-Type': 'application/json',
+      }),
+
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
       .then((data) => {
-        console.log('Login response: ', data);
         if (data.user) {
           localStorage.setItem('user', JSON.stringify(data.user));
           localStorage.setItem('token', data.token);
           onLoggedIn(data.user, data.token);
         } else {
-          alert('No such user found.');
+          alert('Login failed');
         }
       })
-      .catch((e) => {
+      .catch((error) => {
         alert('Something went wrong');
       });
   };
-
   return (
-    <Form onSubmit={handleSubmit} className="text-white">
-      <Form.Group controlId="formUsername">
-        <Form.Label>Username: </Form.Label>
-        <Form.Control
-          type="text"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Form.Group controlId="formPassword">
-        <Form.Label>Password: </Form.Label>
-        <Form.Control
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          required
-        />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
+    <Form onSubmit={handleSubmit} variant="dark">
+      <Row>
+        <Col
+          sm={{ offset: 2 }}
+          md={{ offset: 4 }}
+          className="fw-bold fs-5 align-self-center mb-2 mt-4"
+        >
+          Login:{' '}
+        </Col>
+      </Row>
+      <Row>
+        <Form.Group
+          as={Col}
+          sm={{ offset: 2 }}
+          md={{ offset: 4 }}
+          controlId="formUsername"
+        >
+          <Form.Label>
+            Username:
+            <Form.Control
+              className="text-bg-dark"
+              type="text"
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
+              required
+              placeholder="Enter your username"
+            />
+          </Form.Label>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Form.Group
+          as={Col}
+          sm={{ offset: 2 }}
+          md={{ offset: 4 }}
+          controlId="formPassword"
+        >
+          <Form.Label>
+            Password:
+            <Form.Control
+              className="text-bg-dark"
+              type="password"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+              required
+              placeholder="Enter your password"
+            />
+          </Form.Label>
+        </Form.Group>
+      </Row>
+      <Row>
+        <Col sm={{ offset: 2 }} md={{ offset: 4 }} className="mt-2">
+          <Button variant="primary" type="submit" className="align-self-center">
+            Submit
+          </Button>
+        </Col>
+      </Row>
+      <Row>
+        <Col sm={{ offset: 2 }} md={{ offset: 4 }} className="mt-4">
+          <Link to={`/signup`}>I don't have and account.</Link>
+        </Col>
+      </Row>
     </Form>
   );
 };
