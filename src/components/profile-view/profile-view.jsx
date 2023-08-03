3b93f9c7054b75ae007f0f2d2c1917f9716f9e11
unknown <reman1999@outlook.com>
2023-08-03 23:03:17 +0200

    Fixed profileview error

--- src/components/profile-view/profile-view.jsx
+++ src/components/profile-view/profile-view.jsx
@@ -14,7 +14,7 @@ export const ProfileView = ({ user, token, setUser, movies, onLogout }) => {
   const [birthday, setBirthday] = useState(user.BirthDate);
   const [showModal, setShowModal] = useState(false);
   const favoriteMovies = movies.filter((movie) => {
-    return user.FavoriteMovies.includes(movie.id);
+    return user.favoriteMovies.includes(movie.id);
   });
 
   const handleShowModal = () => setShowModal(true);
